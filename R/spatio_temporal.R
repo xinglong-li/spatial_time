c9520db4cffb922a85ae201f81170827d46639ab
xinglong <xinglong.li@stat.ubc.ca>
2023-06-26 00:34:50 -0700

    wip initialize

--- R/spatio_temporal.R
+++ R/spatio_temporal.R
@@ -16,12 +16,11 @@ spatial_temporal <- function(formula = NULL,
   }
   
   st_mesh <- inla.mesh.2d(loc = coodinates, # Coordinates and borders are to be inputted by user
-                          loc.domain = borders,
+                          boundary = boundary,
                           max.edge = c(),
+                          min.angle = .
                           offset = c(),
                           cutoff = c())
-  st_spde <- inla.spde2.matern(mesh = st_mesh,
-                               alpha = 2)
   # Instead we define the spde also providing pc-prior
   st_spde <- inla.spde2.pcmatern(mesh = mesh,
                                  alpha = 2,
@@ -46,7 +45,7 @@ spatial_temporal <- function(formula = NULL,
                                        n.groups = n_days)
   }
   
-  stack_est_A <- list(A_est, 1)
+  stack_est_A <- list(A_est, 1) # we can put all covariates into one data frame and share the '1'
   stack_est_effects <- list(c(grid_index, list(Intercept = 1)),
                             list(covariates))
   stack_est <- inla.stack(data = list(y = NULL),
@@ -138,8 +137,8 @@ spatial_predict <- function() {
   # If the linear predictor marginal distributions are not necessary, 
   # a less computationally heavy solution consists in projecting the latent field 
   # – estimated at the mesh vertices – onto the grid locations.
-  A_pred <- inla.spde.make.A(mesh=Swiss.mesh)
-  stack_pred_A <- list()
+  A_pred <- inla.spde.make.A(mesh=Swiss.mesh) # Or this is equivalent to A = 1, so that its just on mesh point? page101
+  stack_pred_A <- list() 
   stack_pred_effects <- list()
   stack_pred <- inla.stack(data = list(y = NA),
                            effects = stack_pred_effects,
@@ -149,7 +148,11 @@ spatial_predict <- function() {
                       family = "",
                       data = inla.stack.data(),
                       control.predictor = list(A = inla.stack.A(stack_joint),
-                                               compute = TRUE)
+                                               compute = TRUE),
+                      control.compute = list(config = TRUE),
+                      quantiles = NULL,
+                      control.results = list(return.marginals.random = FALSE,
+                                             return.marginals.predictor = FALSE)
                       )
   index_pred <- inla.stack.index(stack_joint, "prediction")$data
   post_mean_pred <- pred_out$summary.linear.predictor[index.pred, "mean"]
@@ -159,14 +162,21 @@ spatial_predict <- function() {
                                    points)
   
   mean_pred <- inla.mesh.project(proj_grid, post_mean_pred)
+  sd_pred <- inla.mesh.project(proj_grid, post_sd_pred)
   
   # Taking into account the uncertainty by sampling from its posterior distributions.
   # This can be done useing inla.posterior.sample function which needs the output from the inla
   # function by setting config = TRUE
   
+  
   s1 <- inla.posterior.sample(n = 1, result = pred_output)
   
   # Since all the latent field components are stacked, we need to query the index for each component.
+  
+  # Getting samples from the posterior distribution of the linear predictor at mesh nodes.
+  # Then these values are interpolated to the grid and the expected value computed in the response 
+  # scale by applying the inverse of the link function.
+  pred.nodes <- exp(sapply(s1, function(x) x$latent[index_pred]))
 }
 
 
