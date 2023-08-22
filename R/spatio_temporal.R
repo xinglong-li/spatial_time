fac8d2f6e5332e75abb869ccfbe77c715978554b
xinglong <xinglong.li@stat.ubc.ca>
2023-08-21 23:36:19 -0700

    start building package

--- R/spatio_temporal.R
+++ R/spatio_temporal.R
@@ -1,21 +1,48 @@
+# This files contains the main function for fitting a preferential sampling model.
+# 
+
 library(INLA)
 
 
-# spatial_temporal <- function(formula = NULL,
-#                              data = NULL,
-#                              coordicates = NULL,
-#                              boundary = NULL,
-#                              family = NULL) {
-
-spatial_temporal <- function(st_formula_joint, 
-                             family = c('gaussian','binomial','gaussian','gaussian'),
-                             data = inla.stack.data(stack_joint2),
-                             Ntrials=inla.stack.data(stack_joint2)$Ntrials,
-                             control.predictor = list(A = inla.stack.A(stack_joint2), compute = F),
-                             verbose = TRUE) {
+spatio_temporal <- function(formula_observation,
+                            data,
+                            sites,
+                            locations,
+                            times = NULL,
+                            boundary = NULL,
+                            formula_selection = NULL,
+                            cov_observation = NULL,
+                            cov_selection = NULL,
+                            family = "gaussian") {
   # Args:
-  #   family: a string that specifies the distribution of the data.
-  #           Check \code{names(inla.models()$likelihood)} for the full list acceptable families.
+  #   formula_observation: Formula for observation model. Use the syntax of the package lmer.
+  #   formula_selection:   Formula for site selection model. Use the syntax of the package lmer.
+  #   cov_observation:     List of covariance matrix for random effects in the observation model.
+  #                        One for each set of random effects specified in formula_observation.
+  #                        If 'Mattern' used, then spatial SPDE mattern covariance for the 
+  #                        complete mesh built for INLA (not only on observed sites) is used.
+  #                        If not speficied, the diagonal covariance matrix is used for each
+  #                        set of random effects.
+  #   cov_selection:       List of covariance matrix for random effects in the site selection model.
+  #                        One for each set of random effects specified in formula_selection.
+  #                        If 'Mattern' used, then spatial SPDE mattern covariance for the 
+  #                        complete mesh built for INLA (not only on observed sites) is used.
+  #                        If not speficied, the diagonal covariance matrix is used for each
+  #   data:                The data frame contains observation variable and covariates.
+  #                        Each row is the observation of one site at one time point.
+  #   sites:               The site names used as indicator of each site. 
+  #                        Specified by the name of the column in data.
+  #   times:               Time points used as indicator of the time points of each observation.
+  #                        Specified by the name of the column in data.
+  #                        If not specified, the whole observation is treated as from one time point,
+  #                        and only a spatial model (no temporal trend) can be fitted.
+  #   locations:           Locations of sites. It can be either a SpatialPoints object from 
+  #                        package 'sp', or a data frame with each row contains site_name, loc_N, loc_E.
+  #   boundary:            Boundary of sites, must be a SpatialPolygons object from the package 'sp'.
+  #   family:              String that specifies the distribution of the observation.
+  #                        The default family for site selection is always 'Bernoulli'.
+  #                        Check ```names(inla.models()$likelihood)``` for the full list of 
+  #                        acceptable families.
   
   # Prepare standardized variables/factors and for INLA formula ------------------------------------
 
